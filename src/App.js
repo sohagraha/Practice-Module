0457e575215494a98cc724acdec0b0c842fe89d1
sohagraha <sohagrahabd@gmail.com>
2022-09-17 21:16:37 +0600

    all possible result

--- src/App.js
+++ src/App.js
@@ -2,22 +2,145 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const products = [
+  //   { apple: 25, Banana: 30, Orange: 35 },
+  //   { apple: 25, Banana: 30 },
+  //   { apple: 25, Banana: 30 }
+  // ]
+  // const result = {}
+  // products.forEach(product => {
+  //   const prod = product;
+  //   for (const key in prod) {
+  //     if (result[key]) {
+  //       result[key] = result[key] + prod[key]
+  //     }
+  //     else {
+  //       result[key] = prod[key]
+  //     }
+  //   }
+  // })
+  // console.log(result);
+
+  // const products = [
+  //   [{ apple: 25, Banana: 30, Orange: 35 }, { apple: 25, Banana: 30, Orange: 35 }],
+  //   [{ apple: 25, Banana: 30 }, { apple: 25, Banana: 30 }],
+  //   [{ apple: 25, Banana: 30 }, { apple: 25, Banana: 30 }]
+  // ]
+  // const result = {}
+  // products.forEach(product => {
+  //   const prod = product;
+  //   prod.forEach(prod => {
+  //     for (const key in prod) {
+  //       if (result[key]) {
+  //         result[key] = result[key] + prod[key]
+  //       }
+  //       else {
+  //         result[key] = prod[key]
+  //       }
+  //     }
+  //   })
+  // })
+  // console.log(result);
+
+  // const products = [
+  //   {saturday : [{ apple: 25, Banana: 30, Orange: 35 }, { apple: 25, Banana: 30, Orange: 35 }],
+  //   sunday : [{ apple: 25, Banana: 30 }, { apple: 25, Banana: 30 }],
+  //   monday : [{ apple: 25, Banana: 30 }, { apple: 25, Banana: 30 }]}
+  // ]
+  // const result = {}
+  // products.forEach(product => {
+  //   const prod = product;
+  //   for (const key in prod) {
+  //     const prod1 = prod[key];
+  //     prod1.forEach(prod => {
+  //       for (const key in prod) {
+  //         if (result[key]) {
+  //           result[key] = result[key] + prod[key]
+  //         }
+  //         else {
+  //           result[key] = prod[key]
+  //         }
+  //       }
+  //     })
+  //   }
+  // })
+  // console.log(result);
+  
+  // const products = [
+  //   {saturday : { apple: 25, Banana: 30, Orange: 35 },
+  //   sunday : { apple: 25, Banana: 30 },
+  //   monday : { apple: 25, Banana: 30 }}
+  // ]
+  // const result = {}
+  // products.forEach(product => {
+  //   const prod = product;
+  //   for (const key in prod) {
+  //     const prod1 = prod[key];
+  //     for (const key in prod1) {
+  //       if (result[key]) {
+  //         result[key] = result[key] + prod1[key]
+  //       }
+  //       else {
+  //         result[key] = prod1[key]
+  //       }
+  //     }
+  //   }
+  // })
+  // console.log(result);
+
+  // const products = [
+  //     { apple: 25, Banana: 30, Orange: 35 },
+  //     { apple: 25, Banana: 30 },
+  //     { apple: 25, Banana: 30 }
+  //   ];
+
+  //   const result = {}
+
+  // products.forEach(product=>{
+  //   for(const key in product)
+  //   {
+  //     if(result[key])
+  //     {
+  //       result[key] = result[key] + product[key]
+  //     }
+  //     else{
+  //       result[key] = product[key]
+  //     }
+  //   }
+  // })
+  // console.log(result)
+
+//   const products = [
+//       [{ apple: 25, Banana: 30, Orange: 35 }, { apple: 25, Banana: 30, Orange: 35 }],
+//       [{ apple: 25, Banana: 30 }, { apple: 25, Banana: 30 }],
+//       [{ apple: 25, Banana: 30 }, { apple: 25, Banana: 30 }]
+//     ]
+// const result = {};
+
+//   products.forEach(product=>{
+//     product.forEach(prod=>{
+//         for(const key in prod){
+//           if(result[key])
+//           {
+//             result[key] = result[key] + prod[key];
+//           }
+//           else{
+//             result[key] = prod[key];
+//           }
+//         }
+//     })
+//   })
+
+//   console.log(result)
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
